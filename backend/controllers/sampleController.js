68e75cace0a73632165db431ede374881e6946bf
lucagrosso26 <lucagross0426@gmail.com>
2026-06-16 04:25:48 +0000

    Se edito el mensaje que pedia la consgina 9 para el error 404 y posteriormente se creo el test que intenta eliminar un sample ya borrado o que no existe. Para los mismos, se modificaron dos archivos, samplecontroller.js para editar el mensaje y sampletests.js en el frontend para el test íntegro y su boton.

--- backend/controllers/sampleController.js
+++ backend/controllers/sampleController.js
@@ -84,8 +84,8 @@ class SampleController
             // 1. Obtener metadatos para conocer la ruta del archivo físico
             const sample = await sampleRepo.findById(id, userId);
             
-            if (!sample) {
-                return res.status(404).json({ message: "El sample no existe o no tienes permisos para eliminarlo." });
+            if (!sample) {      //edicion por el mensaje pedido
+                return res.status(404).json({ message: "El registro no existe o ya fue eliminado." });
             }
 
             // 2. Ejecutar sp_delete_sample en la base de datos
